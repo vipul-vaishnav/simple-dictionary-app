e44e082282afa2baaf3a9f79315d40299fb25234
noob-coder777 <a45295694@gmail.com>
2023-02-03 01:08:31 +0530

    ADD: set port to 3000

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { MantineProvider, Button } from '@mantine/core'
 import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Button>Click</Button>
+    </MantineProvider>
   )
 }
 
